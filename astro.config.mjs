7f26e2c77ee32d3a4b8458f749d3a9b7f5abb532
Ibrahim Ansari <6884828+retrixe@users.noreply.github.com>
2025-01-05 15:35:42 +0530

    Enable prefetching for all links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  prefetch: {
+    prefetchAll: true,
+  },
   experimental: {
     svg: true,
-  }
+  },
 })
